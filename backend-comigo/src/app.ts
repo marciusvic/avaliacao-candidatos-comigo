4eb943ad1893cc5b4d09519c167443e4f80c3e6d
Marcio Victor <marcio9769@gmail.com>
2024-09-10 18:31:07 -0300

    feat: instaled cors, so now i can make requests

--- backend-comigo/src/app.ts
+++ backend-comigo/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import ticketRoutes from './routes/ticketRoutes';
 import authRouter from './routes/authRoutes';
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+  }));
 app.use(express.json());
 
 app.use("/api", ticketRoutes);
