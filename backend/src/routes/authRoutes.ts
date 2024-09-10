f9b61f05b67b1937e98f57fbe7acf8f69158f9b2
Marcio Victor <marcio9769@gmail.com>
2024-09-10 13:43:53 -0300

    feat: created auth routes and services

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from 'express';
+import { registerUser, loginUser } from '../services/authService';
+
+// Rotas para autenticação
+const router = express.Router();
+
+router.post('/register', async (req: Request, res: Response) => {
+    const { name, email, password, role } = req.body;
+    console.log(req.body);
+    try {
+      const user = await registerUser(name, email, password, role);
+      res.status(201).json(user);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({ error: error.message });
+      } else {
+        res.status(400).json({ error: 'Erro desconhecido' });
+      }
+    }
+});
+
+router.post('/login', async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const { token, user } = await loginUser(email, password);
+
+    if (user) {
+      const { password, ...userWithoutPassword } = user;
+      res.json({ token, user: userWithoutPassword });
+    } else {
+      res.status(400).json({ error: 'Usuário não encontrado' });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(400).json({ error: 'Erro desconhecido' });
+    }
+  }
+});
+
+
+export default router;
